1c164eb0c1ff91117f680c6e9fee5cf0a5cfde95
donleyg <donleyg@users.noreply.github.com>
2020-12-13 23:00:22 -0500

    relative freq

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Going through textbook statistics on jupyter notebooks. ',
+    author='Donley Gustave',
+    license='MIT',
+)
